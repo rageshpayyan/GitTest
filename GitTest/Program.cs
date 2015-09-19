4b274c5ea040a8bcbe51cb3398ee7cd4966ae7f8
rageshpayyan <rageshpayyan@gmail.com>
2015-09-19 09:42:58 -0700

    included gitClass3 project

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using gitClass2;
+using gitClass3;
 namespace GitTest
 {
     class Program
     {
         static void Main(string[] args)
         {
-            gitClass2.Class1 obj = new Class1();
+            gitClass3.Class1 obj = new Class1();
             obj.fun1();
 
         }
