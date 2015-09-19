dc256cdaeee33f1fe224c1cc67a84f1e30e99c6d
rageshpayyan <rageshpayyan@gmail.com>
2015-09-19 09:28:22 -0700

    first test

---
+++ GitTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using gitClass2;
+namespace GitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            gitClass2.Class1 obj = new Class1();
+            obj.fun1();
+
+        }
+    }
+}
